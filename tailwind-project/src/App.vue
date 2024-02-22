610467f21caed9e51fc82ec35e44a6d8175ec741
Montags24 <adrianmontagu@hotmail.co.uk>
2024-02-22 20:17:00 +0000

    messing around

--- tailwind-project/src/App.vue
+++ tailwind-project/src/App.vue
@@ -1,8 +1,27 @@
 <template>
   <main>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    nav
+    <div class="container mx-auto">
+      <div class="font-sans text-5xl font-semibold p-3 block text-center">
+        A utility-first CSS framework for
+        <span class="text-teal-500">rapidly building custom designs.
+        </span>
+      </div>
+      <div>
+        <p class="text-slate-600 text-xl text-center">
+          Tailwind CSS is a highly customizable, low-level CSS framework that gives you all of the building blocks you
+          need to build bespoke designs without any annoying opinionated styles you have to fight to override.
+        </p>
+      </div>
+      <div class="p-2 flex justify-center">
+        <button class="bg-teal-500 hover:bg-teal-600 text-white font-bold py-3 px-4 rounded shadow-md mx-2">
+          Get Started
+        </button>
+        <button class="bg-white hover:bg-gray-100 text-black font-semibold py-3 px-4 rounded shadow-md mx-2">
+          Why Tailwind?
+        </button>
+      </div>
+    </div>
   </main>
 </template>
 
